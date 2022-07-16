c7c808f7afc140f4d751a41022adb59a2ce319e0
seba6453 <sebastian.astudillo@alumnos.ucn.cl>
2022-07-16 11:09:02 -0400

    test

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
+CORS(app)
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# GET, POST, PUT, DELETE
+@app.route('/<usuario>',methods=['GET'])
+def indexGet(usuario):
+    return 'hola ' + usuario
 
+@app.route('/<usuario>',methods=['POST'])
+def indexPost(usuario):
+    return 'hola '
+
+@cross_origin
+@app.route('/File', methods=['POST','OPTIONS'])
+def file():
+    print(request.data)
+    return {"respuesta":"Archivo cargado"}
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
