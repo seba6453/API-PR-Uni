f6c930b69dbea6e23f3fe56d3eaf3c827c84df7e
seba6453 <sebastian.astudillo@alumnos.ucn.cl>
2022-07-18 22:28:00 -0400

    Update main.py
    Ajustes varios al main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import os
+import json
 from cargaDBscotiabank import cargaScotiabank
 
 app = Flask(__name__)
@@ -16,7 +17,8 @@ def index():
 @cross_origin
 @app.route('/File', methods=['POST','OPTIONS'])
 def file():
-    confirmacion = cargaScotiabank(rut=request.data[1].decode('utf8'),datos=request.data[0].decode('utf8'),nroCuenta=111)
+    datos = json.loads(request.data.decode('utf8'))
+    confirmacion = cargaScotiabank(rut=datos["Rut"],datos=datos["InfText"],nroCuenta=111)
     return {"respuesta":confirmacion}
 
 if __name__ == '__main__':
